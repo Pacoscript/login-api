e951d18ce4d0b03d1723002b19470810fe8ed41b
Paco <franfercant@gmail.com>
2019-11-11 23:39:30 +0100

    added basic packages

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const expressSession = require('express-session')
 
-const app = new express();
+const app = new express()
 mongoose.connect(
   process.env.DB_URI,
   { useNewUrlParser: true },
   { useUnifiedTopology: true }
-);
+)
+
+const MongoStore = require('connect-mongo')(expressSession)
+
+app.use(
+  expressSession({
+    secret: process.env.EXPRESS_SESSION_KEY,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
+  })
+)
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`)
+})
