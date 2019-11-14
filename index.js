e5e699b818bbbfbe4d1ac47f8a3f0c3e6955fdf5
Paco <franfercant@gmail.com>
2019-11-14 23:25:53 +0100

    registration configured

--- index.js
+++ index.js
@@ -2,24 +2,38 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const expressSession = require('express-session')
+const connectMongo = require('connect-mongo')
+const bodyParser = require('body-parser')
+
+const createUserController = require ('./controllers/createUser')
+const storeUserController = require('./controllers/storeUser')
 
 const app = new express()
 mongoose.connect(
   process.env.DB_URI,
-  { useNewUrlParser: true },
-  { useUnifiedTopology: true }
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 )
 
-const MongoStore = require('connect-mongo')(expressSession)
+const mongoStore = connectMongo(expressSession)
 
 app.use(
   expressSession({
     secret: process.env.EXPRESS_SESSION_KEY,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection
     }),
+    resave: true,
+    saveUninitialized: true
   })
 )
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
+
+app.get('/auth/register', redirectIfAuthenticated, createUserController)
+app.post('/users/register',  storeUserController)
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on port ${process.env.PORT}`)
